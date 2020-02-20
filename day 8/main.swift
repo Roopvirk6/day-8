3ec6e1db47f04660a5840f95e12cc1f35ee3cfca
ikroop <ikroop>
2020-02-20 18:29:55 -0500

    Initial Commit

---
+++ day 8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day 8
+//
+//  Created by MacStudent on 2020-02-20.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
